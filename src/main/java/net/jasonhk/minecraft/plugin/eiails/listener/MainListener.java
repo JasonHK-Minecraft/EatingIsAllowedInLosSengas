8ba431ff2963248b797856d8c8ecaf57b8b7cbeb
Jason Kwok <JasonHK@users.noreply.github.com>
2020-07-16 23:23:47 +0800

    feat: shortened the visable time of the food level of 19

--- src/main/java/net/jasonhk/minecraft/plugin/eiails/listener/MainListener.java
+++ src/main/java/net/jasonhk/minecraft/plugin/eiails/listener/MainListener.java
@@ -1,10 +1,13 @@
 package net.jasonhk.minecraft.plugin.eiails.listener;
 
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
 
 import lombok.val;
 
+import net.jasonhk.minecraft.plugin.eiails.EatingIsAllowedInLosSengas;
 import net.jasonhk.minecraft.plugin.eiails.event.PlayerStartEatingEvent;
 
 public final class MainListener implements Listener
@@ -16,6 +19,12 @@ public final class MainListener implements Listener
         if (player.getFoodLevel() == 20)
         {
             player.setFoodLevel(19);
+
+            val scheduler = Bukkit.getScheduler();
+            scheduler.scheduleSyncDelayedTask(
+                    JavaPlugin.getPlugin(EatingIsAllowedInLosSengas.class),
+                    () -> { player.setFoodLevel(20); },
+                    2L);
         }
     }
 }
