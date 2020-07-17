52875124c0491ef9d6fbd709cef5c293888f97c8
Jason Kwok <JasonHK@users.noreply.github.com>
2020-07-17 15:42:15 +0800

    refactor: added `@NonNull` annotation

--- src/main/java/net/jasonhk/minecraft/plugin/eiails/event/PlayerStartEatingEvent.java
+++ src/main/java/net/jasonhk/minecraft/plugin/eiails/event/PlayerStartEatingEvent.java
@@ -4,22 +4,26 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.player.PlayerEvent;
 
+import lombok.NonNull;
+
 public class PlayerStartEatingEvent extends PlayerEvent
 {
     private static final HandlerList HANDLERS = new HandlerList();
 
-    public PlayerStartEatingEvent(final Player player)
+    public PlayerStartEatingEvent(@NonNull final Player player)
     {
         super(player);
     }
 
     @SuppressWarnings("unused")
+    @NonNull
     public static HandlerList getHandlerList()
     {
         return HANDLERS;
     }
 
     @Override
+    @NonNull
     public HandlerList getHandlers()
     {
         return HANDLERS;
